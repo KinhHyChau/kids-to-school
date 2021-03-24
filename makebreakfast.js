6ea097034eb88f219bf89acc3b98d1e3b47d4634
KinhHyChau <xiaiyi99@gmail.com>
2021-03-24 17:09:19 -0600

    add makebreakfast.js

---
+++ makebreakfast.js
@@ -0,0 +1,54 @@
+
+/**************************************************************/ 
+/*Below is function of makeBreakfast subprocesses, use       */ 
+/*module.exports to separate from the main kids2school5.js  */
+/***********************************************************/ 
+
+/****************************************************************/ 
+/* require function for prepareIngredients                     */ 
+/* require function for boilWater                             */ 
+/*************************************************************/
+
+const { prepareIngredients} = require('./randompickingredients')
+
+const {boilWater} = require('./boilwater')
+
+function mixIngredientsWithBoilingWater(){
+    console.log("Mix ingredients into boiling water pot")
+}
+
+function setStove(dialValue){
+    console.log("Set stove level to", dialValue)
+}
+
+function waitForCook(minutes){
+    console.log("Waiting to cook for", minutes, "minutes until boiling gain")
+}
+
+/****************************************************************/ 
+/* breakfastReady needs invoking in else statement             */ 
+/* in function kidsToSchool                                   */ 
+/*************************************************************/
+
+function breakfastReady(){
+    console.log("Breakfast is ready to enjoy!") 
+}
+
+function makeBreakfast(areAllIngredientsReady, isWaterBoiled){
+    console.log("Make breakfast")
+        if (!areAllIngredientsReady){
+            prepareIngredients()
+        }
+        if (!isWaterBoiled){
+            boilWater(6)
+        }
+    mixIngredientsWithBoilingWater()
+    setStove(5)
+    waitForCook(8)
+    breakfastReady()
+}
+
+module.exports = {
+    breakfastReady,
+    makeBreakfast
+}
